864075a9089f37bd228956bc4fce29e6360babe4
butterg12 <butterf12.12@gmail.com>
2021-07-20 18:02:19 +0900

    chore: VueJS 프로젝트 폴더 누락된 것 추가, application.properties OSIV false 추가

---
+++ src/frontend/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    outputDir: '../main/resources/static',
+    devServer: {
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                ws: true,
+                changeOrigin: true
+            }
+        }
+    }
+}
